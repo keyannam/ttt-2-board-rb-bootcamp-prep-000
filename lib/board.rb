68695a82b86d039df58acf392d73deced3d58418
keyannamayfield@gmail.com <keyannamayfield@gmail.com>
2018-06-12 17:09:52 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,6 @@
 # Define the variable board below.
-board = []
+board = [" ", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 def board
-  board = [" ", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  board =
 end
